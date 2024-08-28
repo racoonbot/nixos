7e8a68f630c4db2244f017f22e4b0ab73cedec6d
EHOT <eykusof@gmail.com>
2024-08-28 14:51:53 +0300

    Init First config

---
+++ configuration.nix
@@ -0,0 +1,108 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ ./hardware-configuration.nix
+ #     <home-manager/nixos>
+    ];
+
+ #home-manager.useUserPackages = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Определите ваше имя хоста.
+  networking.networkmanager.enable = true; # Включите сетевое соединение.
+  
+  time.timeZone = "Europe/Moscow";
+
+  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "ru_RU.UTF-8";
+    LC_IDENTIFICATION = "ru_RU.UTF-8";
+    LC_MEASUREMENT = "ru_RU.UTF-8";
+    LC_MONETARY = "ru_RU.UTF-8";
+    LC_NAME = "ru_RU.UTF-8";
+    LC_NUMERIC = "ru_RU.UTF-8";
+    LC_PAPER = "ru_RU.UTF-8";
+    LC_TELEPHONE = "ru_RU.UTF-8";
+    LC_TIME = "ru_RU.UTF-8";
+  };
+
+  services.xserver.enable = true;
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.blueman.enable = true;
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  services.printing.enable = true;
+
+  hardware.pulseaudio.enable = false;
+  hardware.bluetooth.enable = true; # enables support for Bluetooth
+  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
+
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+  };
+
+  users.users.pppo = {
+    isNormalUser = true;
+    description = "pppo";
+    extraGroups = [ "networkmanager" "wheel" ];
+    home = "/home/pppo";
+    packages = with pkgs; [
+      kdePackages.kate
+    ];
+  };
+
+  programs.firefox.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    wget
+    curl
+    telegram-desktop
+    sigil
+    megacmd
+    micro
+    libreoffice
+    vscode-with-extensions
+    git
+	bluez
+    blueman
+	htop
+	nnn
+	mc
+  # ciscoPacketTracer8
+	vlc
+	clementine
+    gns3-server
+    gns3-gui
+    dynamips
+    libvirt
+  ];
+
+  #nixpkgs.config.allowUnfree = true;
+
+  system.stateVersion = "unstable"; # Ветка
+
+
+
+ # Некондиционные пакеты
+  nixpkgs.config.allowUnfree = true;
+  
+
+ 
+ # Удалить все, кроме n последних системных профилей
+  system.autoUpgrade.enable = true;
+  system.autoUpgrade.allowReboot = true; # Если хотите автоматически перезагружать систему после обновления
+ # system.autoUpgrade.n = 3; # Количество сохраняемых версий
+  
+  
+}
